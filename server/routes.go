18498f14a4c4c19b9d117ffd64cdf4d5b325115d
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-06-04 15:51:47 -0400

    server moved over and working with bootstrap

---
+++ server/routes.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Routes struct {
+	staticDir string
+	apiKey    string
+	apiSecret string
+}
+
+// Route: Data
+type Data struct {
+	Query    string `json:"query"`
+	Datetime string `json:"datetime"`
+}
+
+// /api/data
+func (r Routes) apiDataRoute(resp http.ResponseWriter, req *http.Request) {
+	var data Data
+
+	resp.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+
+	data.Query = "Dummy data"
+	datetime := time.Now().Format("Mon Aug 5 10:17:08 EST 2019")
+	data.Datetime = datetime
+
+	// Reformat data as JSON
+	result, err := json.Marshal(data)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// print result
+	fmt.Println(string(result))
+
+	resp.Header().Set("Content-Type", "application/json")
+	resp.Write(result)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 // Vue.prototype.$apiEndpoint = 'http://localhost:3333/api'
 
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/aura-light-green/theme.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue, { createApp } from '@vue/compat'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+// Optionally install the BootstrapVue icon components plugin
+
+Vue.use(BootstrapVue)
 
 const app = createApp(App)
-app.use(PrimeVue)
 
 app.use(router)
 
